ea08ac1ac5bb94d68943f5e042d6e2eab149c0b9
Yura <yorikyorik25@gmail.com>
2020-04-29 18:53:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
